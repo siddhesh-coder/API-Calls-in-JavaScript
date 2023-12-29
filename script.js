1ff87b80277b3bb098e11cde27b06a0001a8e47c
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2023-12-29 16:56:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const apiUrl = "https://api.example.com/submit";
+//Post example
+// const apiUrl = "https://api.example.com/submit";
 
-const form = document.querySelector("#contact-form");
+// const form = document.querySelector("#contact-form");
 
-const responseGive = document.querySelector("#response-msg");
+// const responseGive = document.querySelector("#response-msg");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  const formData = new FormData(form);
+//   const formData = new FormData(form);
 
-  const responseOption = {
-    method: "POST",
-    body: formData,
-  };
+//   const responseOption = {
+//     method: "POST",
+//     body: formData,
+//   };
 
-  fetch(apiUrl, responseOption)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      responseGive.textContent = data.message;
-    })
-    .catch((error) => {
-      console.error("Error: " + error);
-    });
-});
+//   fetch(apiUrl, responseOption)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       responseGive.textContent = data.message;
+//     })
+//     .catch((error) => {
+//       console.error("Error: " + error);
+//     });
+// });
